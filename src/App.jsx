d82db96d1a6695c26ee33b24f3f6af8e59cd75f5
Mahesh <maheshpatil.jp@gmail.com>
2025-11-05 10:01:50 +0530

    React project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+const element = <h1>Hello, Mahesh!</h1>;
+  return element
+}
+
+export default App
